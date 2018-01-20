947db3b13eec382af2a085be114665174f07cb2a
Shyngys Kassymov <chika.kasymov@gmail.com>
2018-01-20 17:34:33 +0600

    Added naive Swift reimplementation of LinSpell written on C#

---
+++ LinSpell/Models/LinSpell.swift
@@ -0,0 +1,9 @@
+//
+//  LinSpell.swift
+//  LinSpell
+//
+//  Created by Shyngys Kassymov on 19.01.2018.
+//  Copyright © 2018 Shyngys Kassymov. All rights reserved.
+//
+
+import Foundation
---
+++ LinSpell/Models/StreamReader.swift
@@ -0,0 +1,9 @@
+//
+//  StreamReader.swift
+//  LinSpell
+//
+//  Created by Shyngys Kassymov on 19.01.2018.
+//  Copyright © 2018 Shyngys Kassymov. All rights reserved.
+//
+
+import Foundation
